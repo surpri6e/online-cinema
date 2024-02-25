67d3219fb02a372392915deafe2ce6d26e3b20ad
surpri6e <dan.surpri6e@gmail.com>
2024-02-25 20:15:02 +0300

    fix menu

--- src/components/design/Menu/Menu.tsx
+++ src/components/design/Menu/Menu.tsx
@@ -1,8 +1,12 @@
 import './Menu.scss'
 import { Link } from 'react-router-dom';
-import { MOVIES_PAGE_PATH, SERIES_PAGE_PATH, CARTOONS_PAGE_PATH, REGISTRATION_PAGE_PATH } from '../../../paths';
+import { MOVIES_PAGE_PATH, SERIES_PAGE_PATH, CARTOONS_PAGE_PATH, REGISTRATION_PAGE_PATH, YOU_PAGE_PATH } from '../../../paths';
+import { useContext } from 'react';
+import { IsAuthContext } from '../../../context/isAuth';
 
 const Menu = () => {
+  const {user} = useContext(IsAuthContext)
+
   return (
     <div className="menu">
         <input type="checkbox" id="burger-checkbox" className="burger-checkbox"/>
@@ -11,7 +15,13 @@ const Menu = () => {
           <li><Link to={MOVIES_PAGE_PATH} className="menu-item">Movies</Link></li>
           <li><Link to={SERIES_PAGE_PATH} className="menu-item">Series</Link></li>
           <li><Link to={CARTOONS_PAGE_PATH} className="menu-item">Cartoons</Link></li>
-          <li><Link to={REGISTRATION_PAGE_PATH} className="menu-item">Registration</Link></li>
+          {
+            user
+            ?
+            <li><Link to={YOU_PAGE_PATH} className="menu-item">Me</Link></li>
+            :
+            <li><Link to={REGISTRATION_PAGE_PATH} className="menu-item">Registration</Link></li>
+          }
         </ul>
     </div>
   )
